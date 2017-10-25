90166aa783ca096c243a7fef1899cba51845ea80
Kin.LHP <kin@laptop.kinlhp.com>
2017-10-24 23:51:51 -0200

    Get started

---
+++ src/main/java/com/kinlhp/steveapi/SteveApiApplication.java
@@ -0,0 +1,12 @@
+package com.kinlhp.steveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteveApiApplication.class, args);
+	}
+}
