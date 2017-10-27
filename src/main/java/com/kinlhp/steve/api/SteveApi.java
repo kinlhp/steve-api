fd4f5a948f510912f92e1a7131da55540e00e6dc
Kin.LHP <kin@laptop.kinlhp.com>
2017-10-26 23:08:58 -0200

    Refactoring

--- src/main/java/com/kinlhp/steve/api/SteveApi.java
+++ src/main/java/com/kinlhp/steve/api/SteveApi.java
@@ -1,12 +1,12 @@
-package com.kinlhp.steveapi;
+package com.kinlhp.steve.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SteveApiApplication {
+public class SteveApi {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SteveApiApplication.class, args);
+		SpringApplication.run(SteveApi.class, args);
 	}
 }
