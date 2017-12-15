2370992dad9aba7e96c960e0d1298514d5aa1cf3
Kin.LHP <kin@laptop.kinlhp.com>
2017-12-14 23:05:54 -0200

    REST layer

--- src/main/java/com/kinlhp/steve/api/repositorio/RepositorioCredencial.java
+++ src/main/java/com/kinlhp/steve/api/repositorio/RepositorioCredencial.java
@@ -2,8 +2,10 @@ package com.kinlhp.steve.api.repositorio;
 
 import com.kinlhp.steve.api.dominio.Credencial;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @RepositoryRestResource(
 		collectionResourceRel = "credenciais",
@@ -12,4 +14,8 @@ import java.math.BigInteger;
 )
 public interface RepositorioCredencial
 		extends RepositorioAuditavel<Credencial, Credencial, BigInteger> {
+
+	@Override
+	@RestResource(exported = false)
+	<S extends Credencial> List<S> save(Iterable<S> credenciais);
 }
--- src/main/java/com/kinlhp/steve/api/repositorio/RepositorioUf.java
+++ src/main/java/com/kinlhp/steve/api/repositorio/RepositorioUf.java
@@ -3,8 +3,10 @@ package com.kinlhp.steve.api.repositorio;
 import com.kinlhp.steve.api.dominio.Credencial;
 import com.kinlhp.steve.api.dominio.Uf;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @RepositoryRestResource(
 		collectionResourceRel = "ufs",
@@ -13,4 +15,16 @@ import java.math.BigInteger;
 )
 public interface RepositorioUf
 		extends RepositorioAuditavel<Uf, Credencial, BigInteger> {
+
+	@Override
+	@RestResource(exported = false)
+	<S extends Uf> S save(S uf);
+
+	@Override
+	@RestResource(exported = false)
+	<S extends Uf> List<S> save(Iterable<S> ufs);
+
+	@Override
+	@RestResource(exported = false)
+	<S extends Uf> S saveAndFlush(S uf);
 }
