e02f531657a714401cd74f852bd147503d863d3c
Kin.LHP <kin@laptop.kinlhp.com>
2018-04-04 01:08:56 -0300

    Address validations

---
+++ src/main/java/com/kinlhp/steve/api/servico/validacao/ValidacaoEndereco.java
@@ -0,0 +1,20 @@
+package com.kinlhp.steve.api.servico.validacao;
+
+import com.kinlhp.steve.api.dominio.Endereco;
+import com.kinlhp.steve.api.dominio.Permissao;
+
+public abstract class ValidacaoEndereco extends ValidavelAbstrato<Endereco> {
+
+	private static final long serialVersionUID = -3316825255606671038L;
+
+	/**
+	 * {@link com.kinlhp.steve.api.dominio.Credencial} sem {@link com.kinlhp.steve.api.dominio.Permissao} de {@link com.kinlhp.steve.api.dominio.Permissao.Descricao#ADMINISTRADOR}
+	 * não pode incluir {@link Endereco} em {@link com.kinlhp.steve.api.dominio.Pessoa} com perfil de usuário
+	 */
+	protected void validarPessoa() {
+		if (super.dominio.getPessoa() != null
+				&& super.dominio.getPessoa().isPerfilUsuario()) {
+			super.verificarPermissao(Permissao.Descricao.ADMINISTRADOR);
+		}
+	}
+}
---
+++ src/main/java/com/kinlhp/steve/api/servico/validacao/criacao/ValidacaoCriacaoEndereco.java
@@ -0,0 +1,26 @@
+package com.kinlhp.steve.api.servico.validacao.criacao;
+
+import com.kinlhp.steve.api.dominio.Endereco;
+import com.kinlhp.steve.api.servico.validacao.ValidacaoEndereco;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+@Component(value = "beforeCreateEndereco")
+public class ValidacaoCriacaoEndereco extends ValidacaoEndereco {
+
+	private static final long serialVersionUID = 5871765876300306464L;
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Endereco.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object object, Errors errors) {
+		super.dominio = (Endereco) object;
+		super.erros = errors;
+
+		// TODO: 4/4/18 implementar design pattern que resolva essa má prática
+		validarPessoa();
+	}
+}
