6b3e22bee30e8187b928485b85136ba9877259df
Kin.LHP <kin@laptop.kinlhp.com>
2018-04-04 00:48:14 -0300

    E-mail validations

---
+++ src/main/java/com/kinlhp/steve/api/servico/validacao/ValidacaoEmail.java
@@ -0,0 +1,20 @@
+package com.kinlhp.steve.api.servico.validacao;
+
+import com.kinlhp.steve.api.dominio.Email;
+import com.kinlhp.steve.api.dominio.Permissao;
+
+public abstract class ValidacaoEmail extends ValidavelAbstrato<Email> {
+
+	private static final long serialVersionUID = 1405775721033460860L;
+
+	/**
+	 * {@link com.kinlhp.steve.api.dominio.Credencial} sem {@link com.kinlhp.steve.api.dominio.Permissao} de {@link com.kinlhp.steve.api.dominio.Permissao.Descricao#ADMINISTRADOR}
+	 * não pode incluir {@link Email} em {@link com.kinlhp.steve.api.dominio.Pessoa} com perfil de usuário
+	 */
+	protected void validarPessoa() {
+		if (super.dominio.getPessoa() != null
+				&& super.dominio.getPessoa().isPerfilUsuario()) {
+			super.verificarPermissao(Permissao.Descricao.ADMINISTRADOR);
+		}
+	}
+}
---
+++ src/main/java/com/kinlhp/steve/api/servico/validacao/alteracao/ValidacaoAlteracaoEmail.java
@@ -0,0 +1,26 @@
+package com.kinlhp.steve.api.servico.validacao.alteracao;
+
+import com.kinlhp.steve.api.dominio.Email;
+import com.kinlhp.steve.api.servico.validacao.ValidacaoEmail;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+@Component(value = "beforeSaveEmail")
+public class ValidacaoAlteracaoEmail extends ValidacaoEmail {
+
+	private static final long serialVersionUID = -1178798781721939434L;
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Email.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object object, Errors errors) {
+		super.dominio = (Email) object;
+		super.erros = errors;
+
+		// TODO: 4/4/18 implementar design pattern que resolva essa má prática
+		validarPessoa();
+	}
+}
