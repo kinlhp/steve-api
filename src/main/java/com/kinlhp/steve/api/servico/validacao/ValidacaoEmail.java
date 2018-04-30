f21a10c4a732e9030306f6adcb58c38e97a2943e
Kin.LHP <kin@desktop.kinlhp.com>
2018-04-29 21:42:16 -0300

    E-mail validations documentation polishing

--- src/main/java/com/kinlhp/steve/api/servico/validacao/ValidacaoEmail.java
+++ src/main/java/com/kinlhp/steve/api/servico/validacao/ValidacaoEmail.java
@@ -9,7 +9,7 @@ public abstract class ValidacaoEmail extends ValidavelAbstrato<Email> {
 
 	/**
 	 * {@link com.kinlhp.steve.api.dominio.Credencial} sem {@link com.kinlhp.steve.api.dominio.Permissao} de {@link com.kinlhp.steve.api.dominio.Permissao.Descricao#ADMINISTRADOR}
-	 * não pode incluir {@link Email} em {@link com.kinlhp.steve.api.dominio.Pessoa} com perfil de usuário
+	 * não pode incluir, ou alterar, {@link Email} em {@link com.kinlhp.steve.api.dominio.Pessoa} com perfil de usuário
 	 */
 	protected void validarPessoa() {
 		if (super.dominio.getPessoa() != null
