79f1b3a0f13c81265f5423aa392953b92b45b5fa
Kin.LHP <kin@desktop.kinlhp.com>
2018-05-23 18:46:57 -0300

    YearMonth converter (Hibernate compatibility)

--- src/main/java/com/kinlhp/steve/api/dominio/conversao/ImplementacaoConversorMesReferente.java
+++ src/main/java/com/kinlhp/steve/api/dominio/conversao/ImplementacaoConversorMesReferente.java
@@ -3,24 +3,27 @@ package com.kinlhp.steve.api.dominio.conversao;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.io.Serializable;
+import java.sql.Date;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.ZoneId;
 
 @Converter(autoApply = true)
 public class ImplementacaoConversorMesReferente
-		implements AttributeConverter<YearMonth, LocalDate>, Serializable {
+		implements AttributeConverter<YearMonth, Date>, Serializable {
 
-	private static final long serialVersionUID = 5041719727138404385L;
+	private static final long serialVersionUID = 1045792152700877734L;
 
 	@Override
-	public LocalDate convertToDatabaseColumn(YearMonth yearMonth) {
-		return LocalDate.from(yearMonth.atDay(1)
+	public Date convertToDatabaseColumn(YearMonth yearMonth) {
+		final LocalDate localDate = LocalDate.from(yearMonth.atDay(1)
 				.atStartOfDay(ZoneId.systemDefault()));
+		return Date.valueOf(localDate);
 	}
 
 	@Override
-	public YearMonth convertToEntityAttribute(LocalDate localDate) {
-		return YearMonth.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+	public YearMonth convertToEntityAttribute(Date date) {
+		return YearMonth
+				.from(date.toLocalDate().atStartOfDay(ZoneId.systemDefault()));
 	}
 }
